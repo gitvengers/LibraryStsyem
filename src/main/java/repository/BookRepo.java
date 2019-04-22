7c21a516815bfb692f1deed47671fa03fe346b52
이정준 <rhqnrdl625@gmail.com>
2019-04-22 20:06:12 +0900

    Change BookRepo class to BookRepo interface

--- src/main/java/repository/BookRepo.java
+++ src/main/java/repository/BookRepo.java
@@ -1,5 +1,14 @@
 package repository;
 
-public class BookRepo {
+import domain.Book;
 
+import java.util.List;
+
+public interface BookRepo {
+    List<Book> findAll();
+
+    Book findByTitle(String title);
+
+    void addBook(Book book);
 }
+
