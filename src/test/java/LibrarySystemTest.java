8e88f03d23794b08dead1afcf0f46a9c048f0af0
rinha7 <eocjstnals12@gmail.com>
2019-04-22 20:14:39 +0900

    make LibrarySystemTest

--- src/test/java/LibrarySystemTest.java
+++ src/test/java/LibrarySystemTest.java
@@ -1,4 +1,47 @@
+import domain.Book;
+import service.LibraryService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 public class LibrarySystemTest {
+
+    @Test // Test for addbook
+    public void addBookTest(){
+        Book lol = new Book("lol","me","riot",1);
+        LibraryService service = mock(LibraryService.class);
+
+        service.addBookToList(lol);
+        System.out.println(service.serchInCurrentList("lol"));
+
+        verify(service,times(1)).serchInCurrentList("lol");
+    }
+
+    @Test
+    public void rentBookTest(){
+        Book lol = new Book("lol","me","riot",1);
+        Book lol2 = new Book("lol2","me","riot",2 );
+
+        LibraryService service = mock(LibraryService.class);
+
+        service.addBookToList(lol);
+        service.addBookToList(lol2);
+
+        service.rentBookToUser("lol");
+
+        assertThat(service.serchInCurrentList("lol2"),is(lol2));
+    }
 
 }
