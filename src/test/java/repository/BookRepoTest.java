6bc518356814ea6f8a24519bb8a1f65bdabf9d50
이정준 <rhqnrdl625@gmail.com>
2019-04-22 20:30:08 +0900

    Add findAll Test, doNothing Test in BookRepoTest

--- src/test/java/repository/BookRepoTest.java
+++ src/test/java/repository/BookRepoTest.java
@@ -2,9 +2,14 @@ package repository;
 
 import domain.Book;
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.exceptions.base.MockitoException;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -27,5 +32,29 @@ public class BookRepoTest {
         verify(bookRepo, atLeast(1)).findByTitle("리그오브레전드 가이드북");
         assertThat(book.getTitle() ,is("리그오브레전드 가이드북"));
     }
+    @Test
+    public void testAddBookAtRepo(){
+        Book book = mock(Book.class);
+        bookRepo.addBook(book);
+        verify(bookRepo, atLeast(1)).addBook(book);
+    }
 
+    @Test
+    public void testFindAll(){
+        given(bookRepo.findAll()).willReturn(new ArrayList<Book>());
+
+        Book book = new Book();
+        List<Book> books = bookRepo.findAll();
+        bookRepo.addBook(book);
+
+        assertThat(books.size(), is(1));
+    }
+
+    @Test(expected = MockitoException.class)
+    public void testDoNoting(){
+        doNothing().when(bookRepo).findByTitle(anyString());
+        bookRepo.findByTitle("케넨이 감자를케넨");
+        bookRepo.findByTitle("아리가 그걸 어찌아리");
+        bookRepo.findByTitle("라이즈가 놀라면 서프라이즈");
+    }
 }
