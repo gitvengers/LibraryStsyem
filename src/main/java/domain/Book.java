122abb87b2444ac6ecfb544bb3a45e1a24a6d927
DoDaek <dbxor8960@naver.com>
2019-04-22 19:22:30 +0900

    Add getter & changePublisher

--- src/main/java/domain/Book.java
+++ src/main/java/domain/Book.java
@@ -1,11 +1,24 @@
 package domain;
 
-import lombok.Data;
-
-@Data
 public class Book {
     private String title;
     private String author;
     private String publisher;
     private int count;
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public String getPublisher(){
+        return this.publisher;
+    }
+
+    public void changePublisher(String newPublisher){
+        this.publisher = newPublisher;
+    }
 }
